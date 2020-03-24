c97529921ff4d5ef48e28fc9f98a78e9d3e164b0
Green <handkid1986@gmail.com>
2020-03-24 22:33:09 +0800

    Create repo and add files.

---
+++ Promise/Promise.h
@@ -0,0 +1,19 @@
+//
+//  Promise.h
+//  Promise
+//
+//  Created by Green on 2020/3/24.
+//  Copyright © 2020 Green. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Promise.
+FOUNDATION_EXPORT double PromiseVersionNumber;
+
+//! Project version string for Promise.
+FOUNDATION_EXPORT const unsigned char PromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promise/PublicHeader.h>
+
+
---
+++ Sources/Promise.swift
@@ -0,0 +1,153 @@
+//
+//  Promise.swift
+//  Promise
+//
+//  Created by Green on 2020/3/24.
+//  Copyright © 2020 Green. All rights reserved.
+//
+
+import Dispatch
+
+enum PromiseError: Error {
+    case invaild
+}
+
+public struct AsyncExecutor {
+    public static let main = AsyncExecutor()
+    public static let background = AsyncExecutor(qos: .default)
+    
+    private let queue: DispatchQueue
+    
+    public init(qos: DispatchQoS) {
+        queue = DispatchQueue(label: "com.promise.async-executor", qos: qos)
+    }
+    
+    private init() {
+        queue = DispatchQueue.main
+    }
+    
+    public func async(execute work: @escaping () -> Void) {
+        queue.async(execute: work)
+    }
+}
+
+final public class Promise<T> {
+    private var result: Result<T, Error>?
+
+    /// Resolves store the resolve who chains current instance.
+    /// All the resolves will be inovked after sealing result.
+    private var resolves: [(Result<T, Error>) -> Void] = []
+    
+    /// Executor will execute the resolve(s) at specific DispatchQueue.
+    private var executor: AsyncExecutor?
+    
+    public init() {}
+    public init(_ value: T) {
+        result = .success(value)
+    }
+
+    public init(_ error: Error) {
+        result = .failure(error)
+    }
+
+    public func fulfill(_ value: T) {
+        seal(.success(value))
+    }
+
+    public func reject(_ error: Error) {
+        seal(.failure(error))
+    }
+
+    private func seal(_ v: Result<T, Error>) {
+        result = v
+        let work = { self.resolves.forEach{ $0(v) } }
+        guard let executor = executor else {
+            work()
+            return
+        }
+        executor.async(execute: work)
+    }
+
+    private func pipe(resolve: @escaping (Result<T, Error>) -> Void)  {
+        guard let result = result else {
+            resolves.append(resolve)
+            return
+        }
+        let work = { resolve(result) }
+        guard let executor = executor else {
+            work()
+            return
+        }
+        executor.async(execute: work)
+    }
+
+    @discardableResult
+    public func `continue`( _ body: @escaping (Result<T, Error>) -> Void) -> Promise<T> {
+        let rp = Promise<T>()
+        pipe { value in
+            body(value)
+            rp.seal(value)
+        }
+        return rp
+    }
+
+    @discardableResult
+    func map<U>(_ transform: @escaping (T) throws -> U ) -> Promise<U> {
+        let rp = Promise<U>()
+        pipe { value in
+            switch value {
+                case .success(let element):
+                    do {
+                        let newElement = try transform(element)
+                        rp.fulfill(newElement)
+                    }
+                    catch {
+                        rp.reject(error)
+                    }
+                case .failure(let e):
+                    rp.reject(e)
+            }
+        }
+        return rp
+    }
+
+    @discardableResult
+    public func then(_ body: @escaping (T) -> Void) -> Promise<T> {
+        let rp = Promise<T>()
+        pipe {
+            if case .success(let v) = $0 {
+                body(v)
+            }
+            rp.seal($0)
+        }
+        return rp
+    }
+
+    @discardableResult
+    public func `catch`(_ body: @escaping (Error) -> Void) -> Promise<T> {
+        let rp = Promise<T>()
+        pipe {
+            if case .failure(let error) = $0 {
+                body(error)
+            }
+            rp.seal($0)
+        }
+        return rp
+    }
+}
+
+extension Promise {
+    @discardableResult
+    public func dispatch(to executor: AsyncExecutor) -> Promise<T> {
+        self.executor = executor
+        return self
+    }
+}
+
+func firstly<T>(executor: AsyncExecutor? = nil, body: () -> Promise<T>) -> Promise<T> {
+    let p = body()
+    if let executor = executor {
+        p.dispatch(to: executor)
+    }
+    return p
+}
