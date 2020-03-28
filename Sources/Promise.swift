d170a68478500c874c0620b3f389f68a750ad931
Green <handkid1986@gmail.com>
2020-03-29 00:19:32 +0800

    Drop AsyncExecutor and use GCD directly.

--- Sources/Promise.swift
+++ Sources/Promise.swift
@@ -7,28 +7,12 @@
 //
 
 import Dispatch
+extension DispatchQueue {
+    public static let pm_background = DispatchQueue(label: "com.promise.default.background.qeueu", qos: .default)
+}
 
 enum PromiseError: Error {
     case invaild
-}
-
-public struct AsyncExecutor {
-    public static let main = AsyncExecutor()
-    public static let background = AsyncExecutor(qos: .default)
-    
-    private let queue: DispatchQueue
-    
-    public init(qos: DispatchQoS) {
-        queue = DispatchQueue(label: "com.promise.async-executor", qos: qos)
-    }
-    
-    private init() {
-        queue = DispatchQueue.main
-    }
-    
-    public func async(execute work: @escaping () -> Void) {
-        queue.async(execute: work)
-    }
 }
 
 final public class Promise<T> {
@@ -37,9 +21,6 @@ final public class Promise<T> {
     /// Resolves store the resolve who chains current instance.
     /// All the resolves will be inovked after sealing result.
     private var resolves: [(Result<T, Error>) -> Void] = []
-    
-    /// Executor will execute the resolve(s) at specific DispatchQueue.
-    private var executor: AsyncExecutor?
     
     public init() {}
     public init(_ value: T) {
@@ -60,12 +41,7 @@ final public class Promise<T> {
 
     private func seal(_ v: Result<T, Error>) {
         result = v
-        let work = { self.resolves.forEach{ $0(v) } }
-        guard let executor = executor else {
-            work()
-            return
-        }
-        executor.async(execute: work)
+        resolves.forEach{ $0(v) }
     }
 
     private func pipe(resolve: @escaping (Result<T, Error>) -> Void)  {
@@ -73,26 +49,23 @@ final public class Promise<T> {
             resolves.append(resolve)
             return
         }
-        let work = { resolve(result) }
-        guard let executor = executor else {
-            work()
-            return
-        }
-        executor.async(execute: work)
+        resolve(result)
     }
 
     @discardableResult
-    func map<U>(_ transform: @escaping (T) throws -> U ) -> Promise<U> {
+    func map<U>(on: DispatchQueue = .pm_background, _ transform: @escaping (T) throws -> U ) -> Promise<U> {
         let rp = Promise<U>()
         pipe { value in
             switch value {
                 case .success(let element):
-                    do {
-                        let newElement = try transform(element)
-                        rp.fulfill(newElement)
-                    }
-                    catch {
-                        rp.reject(error)
+                    on.async {
+                        do {
+                            let newElement = try transform(element)
+                            rp.fulfill(newElement)
+                        }
+                        catch {
+                            rp.reject(error)
+                        }
                     }
                 case .failure(let e):
                     rp.reject(e)
@@ -102,19 +75,21 @@ final public class Promise<T> {
     }
 
     @discardableResult
-    public func then<E, U: Promise<E>>(_ body: @escaping (T) throws -> U) -> U {
+    public func then<E, U: Promise<E>>(on: DispatchQueue = .pm_background, _ body: @escaping (T) throws -> U) -> U {
         let rp = U()
         pipe {
             switch $0 {
             case .success(let value):
-                do {
-                    let inp = try body(value)
-                    inp.pipe {
-                        rp.seal($0)
+                on.async {
+                    do {
+                        let inp = try body(value)
+                        inp.pipe {
+                            rp.seal($0)
+                        }
                     }
-                }
-                catch {
-                    rp.reject(error)
+                    catch {
+                        rp.reject(error)
+                    }
                 }
             case .failure(let e):
                 rp.reject(e)
@@ -124,46 +99,37 @@ final public class Promise<T> {
     }
 
     @discardableResult
-    public func `catch`(_ body: @escaping (Error) -> Void) -> Promise<T> {
+    public func `catch`(on: DispatchQueue = .main, _ body: @escaping (Error) -> Void) -> Promise<T> {
         let rp = Promise<T>()
-        pipe {
-            if case .failure(let error) = $0 {
-                body(error)
+        pipe { result in
+            on.async {
+                if case .failure(let error) = result {
+                    body(error)
+                }
             }
-            rp.seal($0)
+            rp.seal(result)
         }
         return rp
     }
     
     @discardableResult
-    public func done(_ body: @escaping (T) -> Void) -> Promise<Void> {
+    public func done(on: DispatchQueue = .main, _ body: @escaping (T) -> Void) -> Promise<Void> {
         let rp = Promise<Void>()
-        pipe {
-            switch $0 {
-            case .success(let value):
-                body(value)
-                rp.fulfill(())
-            case .failure(let error):
-                rp.reject(error)
+        pipe { result in
+            on.async {
+                switch result {
+                case .success(let value):
+                    body(value)
+                    rp.fulfill(())
+                case .failure(let error):
+                    rp.reject(error)
+                }
             }
         }
         return rp
     }
 }
 
-extension Promise {
-    @available(*, deprecated, message: "It's not thread-safe, will implement aysnc for it")
-    @discardableResult
-    public func dispatch(to executor: AsyncExecutor) -> Promise<T> {
-        self.executor = executor
-        return self
-    }
-}
-
-func firstly<T>(executor: AsyncExecutor? = nil, body: () -> Promise<T>) -> Promise<T> {
-    let p = body()
-    if let executor = executor {
-        p.dispatch(to: executor)
-    }
-    return p
+func firstly<T, P: Promise<T>>(body: () -> P) -> P {
+    body()
 }
